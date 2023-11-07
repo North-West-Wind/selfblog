0540f3cf4abf4e1519b336e7622f3ca17bd1af85
North-West-Wind <wsyn148@gmail.com>
2023-11-07 16:08:39 +0800

    hide nav in iframe

--- client/pages/NavApp.tsx
+++ client/pages/NavApp.tsx
@@ -3,12 +3,15 @@ import TopBarComponent from "../components/TopBar";
 import SideBarComponent from "../components/SideBar";
 
 const NavApp: React.FC = () => {
+	// If we are inside iframe, hide navigation menu
+	if (window.frameElement) return <></>;
+
 	const [vertical, setVertical] = useState(window.innerWidth < window.innerHeight);
 	useEffect(() => {
 		window.addEventListener("resize", () => {
 			setVertical(window.innerWidth < window.innerHeight);
 		});
-	});
+	}, []);
 
 	return <>
 		{!vertical && <TopBarComponent />}
