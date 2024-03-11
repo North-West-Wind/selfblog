043ecf66db9c6d3c58fdf6f37b126ae84c33800a
NorthWestWind <wsyn148@gmail.com>
2024-03-11 15:26:49 +0800

    use anchor for nav

--- client/components/NavBarItems.tsx
+++ client/components/NavBarItems.tsx
@@ -4,9 +4,15 @@ type Attributes = { sidebar?: boolean };
 
 const NavBarItemsComponent: React.FC<Attributes> = (props: Attributes) => {
 	return <>
-		<div className="button" style={{ marginTop: props.sidebar ? "9vh" : 0 }} onClick={() => window.location.href = "/"}>Home</div>
-		<div className="button" onClick={() => window.location.href = "/about"}>About</div>
-		<div className="button" onClick={() => window.location.href = "/list"}>Posts</div>
+		<div className="button" style={{ marginTop: props.sidebar ? "9vh" : 0 }}>
+			<a href="/">Home</a>
+		</div>
+		<div className="button">
+			<a href="/about">About</a>
+		</div>
+		<div className="button">
+			<a href="/list">Posts</a>
+		</div>
 	</>
 }
 
