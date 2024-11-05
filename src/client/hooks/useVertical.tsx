d2e5965db355edfea5321ddd32b9ccd24c24a1aa
North-West-Wind <wsyn148@gmail.com>
2024-11-05 16:44:51 +0800

    less padding, fixed resize

--- src/client/hooks/useVertical.tsx
+++ src/client/hooks/useVertical.tsx
@@ -4,11 +4,12 @@ const useVertical = () => {
 	const [vertical, setVertical] = useState(false);
 	useEffect(() => {
 		const onresize = () => {
+			console.log("resizing. vertical?", window.innerWidth < window.innerHeight);
 			setVertical(window.innerWidth < window.innerHeight);
-		}
+		};
 		onresize();
 		window.addEventListener("resize", onresize);
-		return window.removeEventListener("resize", onresize);
+		return () => window.removeEventListener("resize", onresize);
 	}, []);
 	return vertical;
 }
